026169802b73233eae709f3037e719745acead56
HJP <HJP@192.168.219.131>
2022-12-23 09:43:36 +0900

    금요일 아침 푸시/221223/홍준표

--- src/main/ict/notice/controller/NoticeController.java
+++ src/main/ict/notice/controller/NoticeController.java
@@ -4,11 +4,13 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import main.ict.common.ChabunUtils;
 import main.ict.common.chabun.service.ChabunService;
 import main.ict.notice.service.NoticeService;
+import main.ict.notice.vo.NoticeVO;
 
 @Controller
 public class NoticeController {
@@ -23,8 +25,19 @@ public class NoticeController {
 	@GetMapping(value="noticeInsertForm")
 	public String noticeInsertForm() {
 		logger.info("noticeInsertForm() 함수 진입 : ");
-		logger.info("Chabun :\n" + ChabunUtils.getNoticeChabun("d", chabunService.getNoticeChabun().getNnum()));
 		return "notice/noticeInsertForm";
 	}//end of noticeInsertForm() method
+	
+	@GetMapping(value="noticeInsert")
+	public String noticeInsert(NoticeVO nvo) {
+		logger.info("noticeInsert() 함수 진입 : ");
+		
+		//채번 매기기
+		nvo.setNnum(ChabunUtils.getNoticeChabun("d", chabunService.getNoticeChabun().getNnum()));
+		
+		//서비스 호출
+		
+		return "";
+	}//end of noticeInsert() method
 	
 }//end of NoticeController class
